7854cae59ca44a3ead229f12267fc218b8f7caf4
apisceva <aljona.gromova@gmail.com>
2022-02-21 04:54:35 -0500

    AL-1: connection to db

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connection
+{
+    class Program
+    {
+        private const string CategoryName = "Category Name";
+        private const string V = "Product Name";
+
+        static void Main(string[] args)
+        {
+            string queryString = @"select
+cat.Name as 'Category Name',
+pr.Name as 'Product Name'
+from
+dbo.Category cat
+left join dbo.Product pr on pr.CategoryID = cat.ID
+where pr.Name is not null
+order by
+cat.Name"; 
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ShopingCart;Integrated Security=True;";
+                                      
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+               // command.Parameters.AddWithValue("@tPatSName", "Your-Parm-Value");
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0}, {1}",
+                        reader[CategoryName], reader[V])); 
+                      
+                    }
+                }
+                finally
+                {
+                    // Always call Close when done reading.
+                    reader.Close();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
